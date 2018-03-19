e30bf47f6a8349bdf6c9b3db298963e3ca72a480
Tim Rankel <tim.rankel@gmail.com>
2018-03-19 16:25:17 -0700

    Clean up and added Delivery object

---
+++ src/models/delivery.ts
@@ -0,0 +1,14 @@
+export class Delivery {
+    driverName: string;
+    driverId: string;
+    clientId: string;
+    run: string;
+    bagToteId: string;
+    slipToteId: string;
+    activity: string;
+    billing: string;
+    timeDelivered: string;
+    dateDelivered: string;
+    text: string;
+    multiLineText: string;
+}
--- src/models/user.ts
+++ src/models/user.ts
@@ -2,19 +2,19 @@ export class Profile {
     userId: string;
     role: string;
     accountId: string;
-    firstName: string
-    middleName: string
-    lastName: string
+    firstName: string;
+    middleName: string;
+    lastName: string;
     phoneNumber: string;
     email: string;
-    suffix: string
-    dateOfBirth: Date
-    address1: string
-    address2: string
-    city: string
-    state: string
-    zipCode: string
-    gender: string
+    suffix: string;
+    dateOfBirth: Date;
+    address1: string;
+    address2: string;
+    city: string;
+    state: string;
+    zipCode: string;
+    gender: string;
 }
 export class AuthUser{
     email: string;
